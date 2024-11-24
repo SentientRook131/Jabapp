d34935d9fb3389403c520c0e5b89d8f447151eb9
FCWY <fcwyzzr@163.com>
2024-11-24 15:48:52 +0800

    样例Object协议

---
+++ include/runtime/object/Object.h
@@ -0,0 +1,30 @@
+//
+// Created by FCWY on 24-11-24.
+//
+
+#pragma once
+#include <typeinfo>
+
+namespace Jabapp::Object{
+    struct Object {
+        enum class TypeTag{
+            BUILTIN,
+            USER_DEFINED
+        };
+
+        const TypeTag tag;
+
+        Object(TypeTag tag): tag(tag) {}
+    };
+
+    template<typename T>
+    struct BuiltInObject: Object {
+        const type_info& type;
+        T value;
+
+        BuiltInObject(T v):
+            Object(Object::TypeTag::BUILTIN),
+            type(type),
+            value(std::move(v)) {}
+    }
+}
